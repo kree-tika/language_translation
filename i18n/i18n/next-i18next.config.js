770ec75aff2928d8a2ab155c7d5a6591ba5b5de3
kreetika20 <019bim020@sxc.edu.np>
2024-02-23 12:01:37 +0545

    language translation

--- i18n/i18n/next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "fr", "jp", "kr"], // Add all supported languages here
-  },
-  localePath: path.resolve("public/locales"),
-};
